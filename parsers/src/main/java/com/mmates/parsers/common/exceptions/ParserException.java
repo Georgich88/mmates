9b810984abccf5713621be6e0e9a53d1abf7147d
Georgich88 <georgeisaev@gmail.com>
2020-04-25 16:35:48 +0700

    Add main parser objects.

---
+++ parsers/src/main/java/com/mmates/parsers/common/exceptions/ParserException.java
@@ -0,0 +1,7 @@
+package com.mmates.parsers.common.exceptions;
+
+public class ParserException extends Exception {
+    public ParserException(String message) {
+        super(message);
+    }
+}
---
+++ parsers/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.mmates.parsers {
+    requires com.mmates.core;
+    requires org.jsoup;
+    requires commons.io;
+    requires org.slf4j;
+}
