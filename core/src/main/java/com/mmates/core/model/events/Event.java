515a7ad651cc191c6dd925f821b95e4e1742f47c
Georgich88 <georgeisaev@gmail.com>
2020-04-25 16:18:49 +0700

    Add Event class accessors and mutators.

--- core/src/main/java/com/mmates/core/model/events/Event.java
+++ core/src/main/java/com/mmates/core/model/events/Event.java
@@ -10,9 +10,51 @@ import java.util.List;
 
 public class Event implements Loadable {
 
+	private String name;
 	private Promotion promotion;
 	private Date date;
 	private List<Fight> fights = new ArrayList<>();
 	private String location = "";
 
+	// Getters and setters
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Promotion getPromotion() {
+		return promotion;
+	}
+
+	public void setPromotion(Promotion promotion) {
+		this.promotion = promotion;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public List<Fight> getFights() {
+		return fights;
+	}
+
+	public void setFights(List<Fight> fights) {
+		this.fights = fights;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
 }
