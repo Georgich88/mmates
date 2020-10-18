f2e92c232c5cdb8efa19123296e9a91c3f73f131
Georgich88 <georgeisaev@gmail.com>
2020-10-18 17:17:10 +0700

    Promotion refactoring

--- core/src/main/java/com/mmates/core/model/promotion/Promotion.java
+++ core/src/main/java/com/mmates/core/model/promotion/Promotion.java
@@ -6,62 +6,73 @@ import com.mmates.core.model.sources.SourceInformation;
 
 import java.util.*;
 
-
 public class Promotion implements Loadable {
 
-	private UUID id;
-	private String name;
-	private List<Event> events = new ArrayList<>();
-	private Map<SourceInformation, String> profiles = new HashMap<>();
+    private UUID id;
+    private String name;
+    private List<Event> events = new ArrayList<>();
+    private Map<SourceInformation, String> profiles = new EnumMap<>(SourceInformation.class);
 
-	// Constructors
+    // Constructors
 
-	public Promotion() {
-	}
+    public Promotion() {
+    }
 
-	// Profiles URLs
+    // Profiles URLs
 
-	@Override
-	public String getUrl(SourceInformation sourceInformation) {
-		return profiles.getOrDefault(sourceInformation, "");
-	}
+    @Override
+    public String getUrl(SourceInformation sourceInformation) {
+        return profiles.getOrDefault(sourceInformation, "");
+    }
 
-	@Override
-	public void setUrl(SourceInformation sourceInformation, String url) {
-		profiles.put(sourceInformation, url);
-	}
+    @Override
+    public void setUrl(SourceInformation sourceInformation, String url) {
+        profiles.put(sourceInformation, url);
+    }
 
-	// Getters and setters
+    // Getters and setters
 
-	public UUID getId() {
-		return id;
-	}
+    public UUID getId() {
+        return id;
+    }
 
-	public void setId(UUID id) {
-		this.id = id;
-	}
+    public void setId(UUID id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public List<Event> getEvents() {
-		return events;
-	}
+    public List<Event> getEvents() {
+        return events;
+    }
 
-	public void setEvents(List<Event> events) {
-		this.events = events;
-	}
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
 
-	public Map<SourceInformation, String> getProfiles() {
-		return profiles;
-	}
+    public Map<SourceInformation, String> getProfiles() {
+        return profiles;
+    }
 
-	public void setProfiles(Map<SourceInformation, String> profiles) {
-		this.profiles = profiles;
-	}
+    public void setProfiles(Map<SourceInformation, String> profiles) {
+        this.profiles = profiles;
+    }
+
+    // Object inherited methods
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Promotion.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("name='" + name + "'")
+                .add("events=" + events)
+                .toString();
+    }
+
 }
