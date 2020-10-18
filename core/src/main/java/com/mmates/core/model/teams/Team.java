15ecef2c67fd247d4ae33eb48ca9a710c93d2b35
Georgich88 <georgeisaev@gmail.com>
2020-10-18 17:21:20 +0700

    Team toString() method

--- core/src/main/java/com/mmates/core/model/teams/Team.java
+++ core/src/main/java/com/mmates/core/model/teams/Team.java
@@ -5,6 +5,7 @@ import com.mmates.core.model.sources.SourceInformation;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.StringJoiner;
 import java.util.UUID;
 
 public class Team implements Loadable {
@@ -23,6 +24,16 @@ public class Team implements Loadable {
     @Override
     public void setUrl(SourceInformation sourceInformation, String url) {
         profiles.put(sourceInformation, url);
+    }
+
+    // Object inherited methods
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Team.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("name='" + name + "'")
+                .toString();
     }
 
 }
