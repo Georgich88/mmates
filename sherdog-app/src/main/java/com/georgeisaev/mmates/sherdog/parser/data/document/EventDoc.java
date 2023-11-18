9b3ee1ce432d3416f790364629f96d67ff222403
George Isaev <georgeisaev@gmail.com>
2023-11-18 09:33:56 -0300

    feat: use short name to store in mongodb

--- sherdog-app/src/main/java/com/georgeisaev/mmates/sherdog/parser/data/document/EventDoc.java
+++ sherdog-app/src/main/java/com/georgeisaev/mmates/sherdog/parser/data/document/EventDoc.java
@@ -1,5 +1,9 @@
 package com.georgeisaev.mmates.sherdog.parser.data.document;
 
+import static com.georgeisaev.mmates.sherdog.parser.data.document.FieldAlias.*;
+
+import java.time.LocalDate;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,25 +11,41 @@ import lombok.ToString;
 import lombok.experimental.FieldDefaults;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.time.LocalDate;
-import java.util.List;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Getter
 @Setter
 @ToString
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@Document(collection = "mmates_sherdog_event")
+@Document(collection = "mmates-sherdog-event")
 public class EventDoc {
 
   @Id String id;
+
+  @Field(name = SHERDOG_URL)
   String sherdogUrl;
+
+  @Field(name = NAME)
   String name;
+
+  @Field(name = PROMOTION)
   PromotionDoc promotion;
+
+  @Field(name = OWNERSHIP)
   String ownership;
+
+  @Field(name = DATE)
   LocalDate date;
+
+  @Field(name = FIGHTS)
   List<FightDoc> fights;
+
+  @Field(name = LOCATION)
   String location;
+
+  @Field(name = VENUE)
   String venue;
+
+  @Field(name = ENCLOSURE)
   String enclosure;
 }
---
+++ sherdog-app/src/main/java/com/georgeisaev/mmates/sherdog/parser/data/document/FieldAlias.java
@@ -0,0 +1,17 @@
+package com.georgeisaev.mmates.sherdog.parser.data.document;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class FieldAlias {
+    public static final String SHERDOG_URL = "sUrl";
+    public static final String NAME = "nm";
+    public static final String PROMOTION = "prm";
+    public static final String OWNERSHIP = "own";
+    public static final String DATE = "dt";
+    public static final String FIGHTS = "fs";
+    public static final String LOCATION = "loc";
+    public static final String VENUE = "vn";
+    public static final String ENCLOSURE = "encl";
+
+}
